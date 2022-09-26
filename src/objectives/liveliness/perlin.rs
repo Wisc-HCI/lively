2eae799a799857945a342917b9b29905d937ce64
Andrew Schoen <schoen.andrewj@gmail.com>
2022-09-26 11:59:28 -0500

    Update perlin.rs

--- src/objectives/liveliness/perlin.rs
+++ src/objectives/liveliness/perlin.rs
@@ -192,6 +192,7 @@ impl Callable<Vector3<f64>> for OrientationLivelinessObjective {
                         .perlin
                         .get([last_time / self.frequency, self.offsets[2]])),
         );
+        println!("noise {:?}",self.noise);
         self.time = Some(time);
     }
 
