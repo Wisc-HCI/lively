45d129dd3e3821f252849f0061605a386e0e4fdf
hunterZh37 <zzhang978@wisc.edu>
2022-12-05 11:14:48 -0600

    working rust finally

---
+++ LivelyDocumentation/docs/tutorial-basics/rust.rs
@@ -0,0 +1,9 @@
+use lively::lively::Solver;
+use lively::objectives::core::base::CollisionAvoidanceObjective;
+use lively::objectives::core::base::SmoothnessMacroObjective;
+use lively::objectives::core::matching::PositionMatchObjective;
+use lively::objectives::objective::Objective;
+use lively::utils::goals::Goal;
+use lively::utils::info::TransformInfo;
+use lively::utils::shapes::*;
+
