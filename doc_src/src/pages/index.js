48d411ffa3c85f1b152f53f1d04b6c4ab6a92139
Andrew Schoen <schoen.andrewj@gmail.com>
2022-12-14 13:15:14 -0600

    Updated banner on doc main

--- doc_src/src/pages/index.js
+++ doc_src/src/pages/index.js
@@ -1,24 +1,46 @@
-import React from 'react';
-import clsx from 'clsx';
-import Link from '@docusaurus/Link';
-import useDocusaurusContext from '@docusaurus/useDocusaurusContext';
-import Layout from '@theme/Layout';
-import HomepageFeatures from '@site/src/components/HomepageFeatures';
+import React from "react";
+import clsx from "clsx";
+import Link from "@docusaurus/Link";
+import useDocusaurusContext from "@docusaurus/useDocusaurusContext";
+import Layout from "@theme/Layout";
+import HomepageFeatures from "@site/src/components/HomepageFeatures";
 
-import styles from './index.module.css';
+import styles from "./index.module.css";
+import bannerUrl from "@site/static/img/doc_banner.png";
 
 function HomepageHeader() {
-  const {siteConfig} = useDocusaurusContext();
+  const { siteConfig } = useDocusaurusContext();
   return (
-    <header className={clsx('hero hero--primary', styles.heroBanner)}>
-      <div className="container">
+    <header
+      className={clsx("hero hero--primary", styles.heroBanner)}
+      style={{
+        backgroundImage: `url(${bannerUrl})`,
+        backgroundPosition:'center',
+        backgroundSize:'cover'
+      }}
+    >
+      <div 
+        className="container" 
+        style={{
+          backgroundColor:'#99999955',
+          padding:15,
+          borderRadius:4,
+          backdropFilter:'blur(5px)',
+          WebkitBackdropFilter:'blur(5px)'
+        }}
+      >
         <h1 className="hero__title">{"Lively v0.10.0 (beta)"}</h1>
-        <p className="hero__subtitle">{"A highly configurable toolkit for commanding robots in mixed modalities"}</p>
+        <p className="hero__subtitle">
+          {
+            "A highly configurable toolkit for commanding robots in mixed modalities"
+          }
+        </p>
         <div className={styles.buttons}>
           <Link
             className="button button--secondary button--lg"
-            to="/docs/API/intro">
-            Lively API 
+            to="/docs/API/intro"
+          >
+            Lively API
           </Link>
         </div>
       </div>
@@ -27,11 +49,12 @@ function HomepageHeader() {
 }
 
 export default function Home() {
-  const {siteConfig} = useDocusaurusContext();
+  const { siteConfig } = useDocusaurusContext();
   return (
     <Layout
       title={""}
-      description="A highly configurable toolkit for commanding robots in mixed modalities">
+      description="A highly configurable toolkit for commanding robots in mixed modalities"
+    >
       <HomepageHeader />
       <main>
         <HomepageFeatures />
