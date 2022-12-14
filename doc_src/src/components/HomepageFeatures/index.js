4b49c7e726368d4ed56f3600935b95d76b8f28e6
Andrew Schoen <schoen.andrewj@gmail.com>
2022-12-14 11:52:26 -0600

    Images and Color

--- doc_src/src/components/HomepageFeatures/index.js
+++ doc_src/src/components/HomepageFeatures/index.js
@@ -4,29 +4,29 @@ import styles from './styles.module.css';
 
 const FeatureList = [
   {
-    title: 'Feature 1',
-    Svg: require('@site/static/img/undraw_docusaurus_mountain.svg').default,
+    title: 'Design Level',
+    Svg: require('@site/static/img/design_level.svg').default,
     description: (
       <>
-        ...
+        The Design Level enables programming robots using a state-based approach.
       </>
     ),
   },
   {
-    title: 'Feature 2',
-    Svg: require('@site/static/img/undraw_docusaurus_tree.svg').default,
+    title: 'Develop Level',
+    Svg: require('@site/static/img/develop_level.svg').default,
     description: (
       <>
-        ...
+        The Develop Level is configurable and portable, usable in applications such as ROS-based control and web-based simulation.
       </>
     ),
   },
   {
-    title: 'Feature 3',
-    Svg: require('@site/static/img/undraw_docusaurus_react.svg').default,
+    title: 'Extend Level',
+    Svg: require('@site/static/img/extend_level.svg').default,
     description: (
       <>
-        ...
+        The Extend Level supports the addition of new characteristics and goal specifications for greater customizability and extendability.
       </>
     ),
   },
