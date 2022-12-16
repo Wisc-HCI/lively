2df271a2819e7830e153ff53d389b1946377473f
hunterZh37 <zzhang978@wisc.edu>
2022-12-16 09:32:32 -0600

    fixed button bug

--- doc_src/src/pages/index.js
+++ doc_src/src/pages/index.js
@@ -38,7 +38,7 @@ function HomepageHeader() {
         <div className={styles.buttons}>
           <Link
             className="button button--secondary button--lg"
-            to="../docs/API/index"
+            to="../docs/API/"
           >
             Lively API
           </Link>
