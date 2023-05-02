871c73eb294d4d4b4849163528c2956b51691b78
Andrew Schoen <schoen.andrewj@gmail.com>
2023-05-02 10:41:40 -0500

    Update index.js

--- docs/src/theme/ReactLiveScope/index.js
+++ docs/src/theme/ReactLiveScope/index.js
@@ -1,5 +1,5 @@
 import React from 'react';
-import init, {Solver} from '../../../../pkg-web';
+import {Solver} from '../../../../pkg';
 // import {Solver} from '@people_and_robots/lively';
 import {panda, ur3e, ur5e, pepper} from './urdfs';
 import { Tree } from './Tree';
